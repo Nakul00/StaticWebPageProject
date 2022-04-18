70ee66b0a220c2a34e4d34c8de185ff73235533f
Nakul <nakulpadhya@gmail.com>
2022-04-17 20:25:52 -0400

    commit
    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+
+import { SurveysComponent } from './surveys/surveys.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    
+    SurveysComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
